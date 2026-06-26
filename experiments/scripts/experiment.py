9ca3cef385fd60f714c2ea144cb62d2e7a6f356c
Marit van den Helder <97402119+Marit-vdh@users.noreply.github.com>
2026-06-26 12:41:53 +0200

    Update responses

--- experiments/scripts/experiment.py
+++ experiments/scripts/experiment.py
@@ -13,7 +13,6 @@ from retrieve import (
     bm25_retriever,
 )
 from generator import generate_response
-from analyse_results import analyse_rag_response
 
 full_directory_name = (
     "/Users/maritvandenhelder/information_studies/thesis/master-thesis/experiments"
@@ -28,19 +27,20 @@ class Experiment:
         embedder: str,
         retriever: str,
         generator: str,
+        ids: list[str],
+        texts: list[str],
         test: bool = True,
         verbose=True,
         load_new_index: bool = True,
         embedding_file: str = "data.tsv",
-        system_prompt_setup: str = "Je bent een behulpzame chatbot voor journalisten. Je antwoordt altijd in het Nederlands. Gebruik alleen de volgende stukken informatie bij het opstellen van een antwoord: ",
     ):
         # Set up global variables
         self.api_key = api_key
         self.embedder = embedder
         self.retriever = retriever
         self.generator = generator
-        self.ids, self.texts = import_articles(flatten=True, test=test)
-        self.system_prompt_setup = system_prompt_setup
+        self.ids = ids
+        self.texts = texts
         self.embedding_file = embedding_file
 
         if load_new_index and embedder != "BM25":
@@ -65,186 +65,6 @@ class Experiment:
                 load_new_index=load_new_index,
             )
 
-    def request_prompt(
-        self, manual_prompt: bool = False, automatic: str = "Amsterdam", verbose=False
-    ):
-        """Ask a user for a prompt and embed the prompt.
-
-        Args:
-            manual_prompt (bool, optional): setting to ask for user input for the prompt through
-                the terminal. If it is set to false, the 'automatic' argument is
-                used as the prompt. Defaults to False.
-            automatic (str, optional): Prompt added in the code itself. Defaults to "Amsterdam".
-        """
-
-        if manual_prompt:
-            self.user_prompt = input("Please enter your prompt: ")
-        else:
-            self.user_prompt = automatic
-
-        if self.embedder != "BM25":
-            print("Embedding user prompt") if verbose else None
-            self.embedded_user_prompt = embed_prompt(
-                model=self.embedder,
-                prompt=self.user_prompt,
-                api_key=self.api_key,
-            )
-
-    def rag(
-        self,
-        manual_prompt: bool = False,
-        automatic="Amsterdam",
-        retriever_func: function = cosine_similarity,
-        content_access: str = "content",
-        n_articles: int = 3,
-        report_to_terminal: bool = False,
-        write: bool = True,
-        prompt_type: str = "question",
-        output_filename: str = "rag_response.json",
-        verbose=False,
-        evaluate_response=True,
-    ):
-        """RAG pipeline with parameters for asking for a user prompt. Has option
-        to write the response to a file and/or the terminal.
-
-        Args:
-            manual_prompt (bool, optional): Setting to ask for user input for the prompt
-                through the terminal. If it is set to false, the 'automatic' argument is
-                used as the prompt. Defaults to False.
-            automatic (str, optional): Prompt added in the code itself. Defaults to "Amsterdam".
-            n_articles (int, optional): Number of articles to use for response generation.
-                Defaults to 3.
-            report_to_terminal (bool, optional): Parameter to print the prompt, used articles and
-                generator response to the terminal. Defaults to False.
-            write (bool, optional): Parameter to write the prompt, used articles and
-                generator response to the output_file in json format. Defaults to True.
-            output_filename (str, optional): Filename to write response to.
-                Defaults to "rag_response.json".
-
-        Returns:
-            str: response from generator.
-        """
-        self.request_prompt(manual_prompt=manual_prompt, automatic=automatic)
-
-        if self.embedder == "BM25":
-            relevant_documents, relevant_document_ids = bm25_retriever(
-                retriever=self.retriever,
-                tokenizer=self.tokenizer,
-                queries=[self.user_prompt],
-                n_articles=n_articles,
-            )
-
-        else:
-            relevant_documents, relevant_document_ids = retrieve_vector(
-                retriever=retriever_func,
-                query=self.embedded_user_prompt,
-                embeddings_file=self.embedding_file,
-                indices=self.ids,
-                texts=dict(zip(self.ids, self.texts)),
-                n_articles=n_articles,
-                verbose=verbose,
-            )
-
-        # Set up the system prompt using the relevant articles
-        system_prompt = (
-            self.system_prompt_setup
-            + f"{'\n'.join([f'{chunk}' for chunk in relevant_documents])}"
-        )
-
-        # Generate a response
-        response = generate_response(
-            api_key=self.api_key,
-            model=self.generator,
-            system_prompt=system_prompt,
-            user_prompt=self.user_prompt,
-            content_access=content_access,
-            verbose=verbose,
-            # max_completion_tokens=250,
-        )
-
-        if evaluate_response:
-            scores = analyse_rag_response(
-                self.user_prompt, relevant_documents, response
-            )
-        else:
-            scores = {}
-
-        if write:
-            with open(output_filename, "w") as output_file:
-                output = {}
-                output["embedder"] = self.embedder_name
-                output["generator"] = self.generator_name
-                output["scores"] = scores
-                output["distance_metric"] = (
-                    None if self.embedder_name == "BM25" else retriever_func.__name__
-                )
-
-                output["user_prompt"] = self.user_prompt
-                output["prompt_type"] = prompt_type
-                output["articles_used"] = {
-                    article_id: article_text
-                    for article_id, article_text in zip(
-                        relevant_document_ids, relevant_documents
-                    )
-                }
-                output["response"] = response
-                json.dump(output, output_file)
-
-        if report_to_terminal:
-            print(f"User prompt:\n{self.user_prompt}\n")
-            print(f"Articles used:\n{relevant_document_ids}\n")
-            print(
-                f"{'\n'.join([f'{docid} - {article}\n' for docid, article in zip(relevant_document_ids, relevant_documents)])}\n"
-            )
-            print(f"\nResponse:\n")
-            print(response)
-
-        return response
-
-    def request_multiple_prompts(
-        self,
-        prompts: list[str],
-        content_access: str = "content",
-        retriever_func: function = cosine_similarity,
-        n_articles: int = 3,
-        verbose=False,
-        directory="../responses",
-        evaluate_response=True,
-    ):
-        """Request multiple responses within the existing experiment from a
-        file containing the prompts.
-
-        Args:
-            prompt_file (str): a file with plaintext formatting containing a new prompt
-             on each line.
-            n_articles (int, optional): Number of articles to use for response generation.
-             Defaults to 3.
-        """
-
-        i = 0
-
-        print("Generate responses based on prompts") if verbose else None
-
-        for prompt in tqdm(prompts):
-
-            prompt_text = prompt[1]
-
-            self.rag(
-                manual_prompt=False,
-                automatic=prompt_text,
-                retriever_func=retriever_func,
-                content_access=content_access,
-                n_articles=n_articles,
-                report_to_terminal=False,
-                write=True,
-                prompt_type=prompt[0],
-                output_filename=f"{directory}/prompt_{i}_response.json",
-                verbose=False,
-                evaluate_response=evaluate_response,
-            )
-
-            i += 1
-
     def generate_responses(
         self,
         prompts: list[tuple[str, str]],
@@ -259,18 +79,26 @@ class Experiment:
 
         for i in tqdm(range(len(prompts))):
 
-            prompt_type = prompts[i][0]
+            system_prompt_setup = prompts[i][0]
             user_prompt = prompts[i][1]
 
             # Retrieve relevant documents from the dictionaries
             relevant_doc_ids = self.relevant_docs[user_prompt][1]
             relevant_docs = self.relevant_docs[user_prompt][0]
 
-            # Set up the system prompt using the relevant articles
-            system_prompt = (
-                self.system_prompt_setup
-                + f"{'\n'.join([f'{chunk}' for chunk in relevant_docs])}"
-            )
+            if len(relevant_doc_ids) == 0:
+                system_prompt = system_prompt = (
+                    "Je bent een tool die gebruikt wordt voor journalistieke doeleinden. Hou je altijd aan de journalistieke code. Antwoord in het Nederlands."
+                    + system_prompt_setup
+                )
+
+            else:
+                # Set up the system prompt using the relevant articles
+                system_prompt = (
+                    "Je bent een tool die gebruikt wordt voor journalistieke doeleinden. Hou je altijd aan de journalistieke code. Antwoord in het Nederlands."
+                    + system_prompt_setup
+                    + f"Gebruik, indien relevant, de volgende artikelen als extra context: {'\n'.join([f'{chunk}' for chunk in relevant_docs])}"
+                )
 
             # Generate a response
             response = generate_response(
@@ -282,11 +110,6 @@ class Experiment:
                 n_completions=5,
             )
 
-            if evaluate_response:
-                scores = analyse_rag_response(user_prompt, relevant_docs, response)
-            else:
-                scores = {}
-
             if write:
                 with open(
                     f"{output_directory}/prompt_{i}_response.json", "w"
@@ -294,15 +117,13 @@ class Experiment:
                     output = {}
                     output["embedder"] = self.embedder_name
                     output["generator"] = self.generator_name
-                    output["scores"] = scores
                     output["distance_metric"] = (
                         None
-                        if self.embedder_name == "BM25"
+                        if self.embedder_name in {"BM25", "no_retrieval"}
                         else self.retriever_func.__name__
                     )
-
+                    output["instruction"] = system_prompt_setup
                     output["user_prompt"] = user_prompt
-                    output["prompt_type"] = prompt_type
                     output["articles_used"] = {
                         article_id: article_text
                         for article_id, article_text in zip(
@@ -352,27 +173,49 @@ def get_prompts(filename):
     return contents
 
 
-def get_model_list(filename):
-    contents = []
+def load_benchmark(filename):
+    with open("../sources/benchmark.json", "r") as benchmark_file:
+        benchmark = json.load(benchmark_file)
+
+    prompts = []
+    # Retrieve the individual prompts from the benchmark
+    for item in benchmark:
+        system_prompt = item["instruction"]
+        model_input = item["input"]
+        choices = (
+            f" Kies uit de volgende opties: {item["choices"]}. Geef alleen een enkele letter als resultaat."
+            if item["choices"] != "-"
+            else ""
+        )
+        instructions = system_prompt + choices
+        prompts.append((instructions, model_input))
+
+    return benchmark, prompts
+
+
+def get_skip_file(filename):
+    contents = set()
     with open(filename) as file:
         for line in file:
-            contents.append(line.strip())
+            contents.add(f"{full_directory_name}/results/{line.strip()}")
     return contents
 
 
 class Experiments:
     def __init__(
         self,
+        skip_file: str,
         embedders_file: str,
         generator_file: str,
         prompt_file: str,
         distances: list[function],
         n_articles: int = 3,
     ):
-
+        self.skip = get_skip_file(skip_file)
+        self.ids, self.texts = import_articles(flatten=True, test=False)
         self.embedders = get_embedders(embedders_file)
         self.generators = get_generators(generator_file)
-        self.prompts = get_prompts(prompt_file)
+        self.benchmark, self.prompts = load_benchmark(prompt_file)
         self.distances = distances
         self.n_articles = n_articles
 
@@ -383,6 +226,11 @@ class Experiments:
             os.makedirs(self.embedding_directory)
 
     def generate(self, experiment, directory):
+
+        # Do not generate new output if the directory is in the skip file
+        if directory in self.skip:
+            print(f"Skipping {experiment.embedder_name} + {experiment.generator_name}")
+            return
 
         if not os.path.exists(directory):
             os.makedirs(directory)
@@ -401,6 +249,9 @@ class Experiments:
             write=True,
             output_directory=directory,
         )
+
+        with open("../sources/skip_file.txt", "a") as skip_file:
+            skip_file.write(f"{directory}\n")
 
     def run(self):
 
@@ -421,6 +272,8 @@ class Experiments:
                 embedder=embedder.strip("/"),
                 retriever="",
                 generator="",
+                ids=self.ids,
+                texts=self.texts,
                 test=False,
                 load_new_index=load_new_index,
                 embedding_file=f"{self.embedding_directory}/{embedder_name}.tsv",
@@ -429,7 +282,31 @@ class Experiments:
 
             experiment.embedder_name = embedder_name
 
-            if experiment.embedder_name == "BM25":
+            if experiment.embedder_name == "no_retrieval":
+
+                print("Testing without RAG.")
+
+                experiment.relevant_docs = {}
+
+                for prompt in tqdm(self.prompts):
+
+                    experiment.relevant_docs[prompt[1]] = (
+                        [],
+                        [],
+                    )
+
+                for generator_tuple in self.generators:
+                    experiment.generator = generator_tuple[0]
+                    experiment.content_access = generator_tuple[1]
+                    experiment.generator_name = generator_tuple[0].split("/")[1]
+
+                    print(f"Using generator {experiment.generator_name}")
+
+                    directory = f"{full_directory_name}/results/no_retrieval/{experiment.generator_name}"
+
+                    self.generate(experiment=experiment, directory=directory)
+
+            elif experiment.embedder_name == "BM25":
 
                 print("Using BM25 retrieval")
 
@@ -509,9 +386,10 @@ if __name__ == "__main__":
 
     print(f"Running experiments")
     experiments = Experiments(
+        "../sources/skip_file.txt",
         "../sources/embedders.csv",
         "../sources/generators.csv",
-        "../sources/prompts.csv",
+        "../sources/benchmark.json",
         [cosine_similarity, manhattan],
     )
     experiments.run()
